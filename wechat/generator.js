be586b7ec0a43ee57b8a33e8880cecc960e718fe
nooldey <openzws@163.com>
2017-08-02 16:51:52 +0800

    change file

--- wechat/generator.js
+++ wechat/generator.js
@@ -1,11 +1,10 @@
 const sha1 = require('sha1')
-const getRawBody = require('raw-body')
-const util = require('../libs/util')
+const handleReq = require('./handle')
 
 const fs = require('fs')
 const path = require('path')
-const err_log = path.join(__dirname,'../logs/error.log')
-const acc_log = path.join(__dirname,'../logs/access.log')
+const err_log = path.join(__dirname, '../logs/error.log')
+const acc_log = path.join(__dirname, '../logs/access.log')
 
 module.exports = (opt) => {
     return async(ctx, next) => {
@@ -15,42 +14,15 @@ module.exports = (opt) => {
         let str = [token, timestamp, nonce].sort().join('');
         let sha = sha1(str);
 
-        // 请求处理
         if (ctx.method === 'GET') {
-            ctx.type = 'text/json';
             ctx.body = (sha === signature) ? echostr + '' : "failed";
-        } else if (ctx.method === 'POST') {
+        }
+        if (ctx.method === 'POST') {
             if (sha !== signature) {
-                ctx.body = "failed";
+                ctx.body = "success";
                 return false;
-            }
-            // xml
-            let data = getRawBody(this.req,{
-                limit: '1MB',
-                encoding: this.charset
-            })
-            let d = util.parseXML(data)
-// console.log('this is content:',d)
-            let msg = util.formatMessage(d)
-// console.log('this is message:',msg)
-fs.writeFileSync(err_log,d+'\n',{flag:'a'})
-
-            if (msg.MsgType === 'event') {
-fs.writeFileSync(err_log,'event\n',{flag:'a'})
-                if (msg.Event === 'subscribe') {
-fs.writeFileSync(err_log,'subscribe\n',{flag:'a'})
-                    let now = Date.now()
-                    ctx.status = 200;
-                    ctx.type = 'application/xml';
-                    ctx.body = `<xml>
-                    <ToUserName><![CDATA[${msg.FromUserName}]]</ToUserName>
-                    <FromUserName><![CDATA[${msg.FromUserName}]]</FromUserName>
-                    <CreateTime>${now}</CreateTime>
-                    <MsgType><![CDATA[text]]</MsgType>
-                    <Content><![CDATA[HEY!Nooldey]]</Content>
-                    </xml>`;
-fs.writeFileSync(err_log,'body:'+ctx.body+'\n',{flag:'a'})                 
-                }
+            } else {
+                handleReq(ctx)
             }
         }
 
@@ -60,17 +32,20 @@ fs.writeFileSync(err_log,'body:'+ctx.body+'\n',{flag:'a'})
             url: ctx.request.url,
             argument: rq,
             method: ctx.method,
-            response: ctx.response,
-            body: ctx.body
+            response: ctx.response
         };
         let log = JSON.stringify(log_content) + "\n";
 
-        if (ctx.response.status!==200) {
-            fs.writeFileSync(err_log,log,{flag:'a'})
+        if (ctx.response.status !== 200) {
+            fs.writeFileSync(err_log, log, {
+                flag: 'a'
+            })
         } else {
-            fs.writeFileSync(acc_log,log,{flag:'a'})
+            fs.writeFileSync(acc_log, log, {
+                flag: 'a'
+            })
         }
-        
+
         await next();
     }
 }
---
+++ wechat/handle.js
@@ -0,0 +1,45 @@
+const getRawBody = require('raw-body')
+const util = require('../libs/util')
+
+const handleReq = ctx => {
+    // xml
+    let data = getRawBody(ctx.req, {
+        limit: '1MB',
+        encoding: this.charset
+    })
+    let d = util.parseXML(data)
+    let msg = util.formatMessage(d)
+
+    if (msg.MsgType === 'event') {
+        if (msg.Event === 'subscribe') {
+            let now = Date.now()
+            ctx.status = 200;
+            ctx.type = 'application/xml';
+            ctx.body = `<xml>
+                    <ToUserName><![CDATA[${msg.FromUserName}]]</ToUserName>
+                    <FromUserName><![CDATA[${msg.FromUserName}]]</FromUserName>
+                    <CreateTime>${now}</CreateTime>
+                    <MsgType><![CDATA[text]]</MsgType>
+                    <Content><![CDATA[HEY!Nooldey]]</Content>
+                    </xml>`;
+        }
+    } else {
+        let now = Date.now()
+        ctx.status = 200;
+        ctx.type = 'application/xml';
+        ctx.body = `<xml>
+                    <ToUserName><![CDATA[${msg.FromUserName}]]</ToUserName>
+                    <FromUserName><![CDATA[${msg.FromUserName}]]</FromUserName>
+                    <CreateTime>${now}</CreateTime>
+                    <MsgType><![CDATA[text]]</MsgType>
+                    <Content><![CDATA[test]]</Content>
+                    </xml>`;
+    }
+
+
+
+}
+
+module.exports = {
+    handleReq
+}
